2d4c84e8e5987f0541bbad41166ffdaa8b48aa9a
heroahmadi <hero.lionel@gmail.com>
2017-12-11 23:58:44 +0700

    Add files via upload

---
+++ ftpclient.py
@@ -0,0 +1,25 @@
+import socket                   
+
+s = socket.socket()             
+host = socket.gethostname()     
+port = 60000                    
+
+s.connect((host, port))
+
+filename = raw_input('What do you want to download? ')
+s.send(filename)
+
+filename = 'received-'+filename
+with open(filename, 'wb') as f:
+    #print 'file opened'
+    while True:
+        print('receiving data...')
+        data = s.recv(1024)
+        if not data:
+            break
+        f.write(data)
+
+f.close()
+print('Successfully get the file')
+s.close()
+print('connection closed')
